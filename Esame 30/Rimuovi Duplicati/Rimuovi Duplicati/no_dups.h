130140b113c5af53679f00504aae007b52dec9d2
robertoreggiani <141009797+robertoreggiani@users.noreply.github.com>
2024-01-07 12:37:02 +0100

    added exam 30

---
+++ Esame 30/Rimuovi Duplicati/Rimuovi Duplicati/no_dups.h
@@ -0,0 +1,12 @@
+#ifndef no_dups_h
+#define no_dups_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+#include "elemtype.h"
+
+
+extern Item *RemoveDuplicates(const Item* i);
+
+#endif /* no_dups_h */
