a68b00d1cffba861ea353c68aeb7400fcf297483
robertoreggiani <141009797+robertoreggiani@users.noreply.github.com>
2024-01-08 21:20:35 +0100

    added Exam 27

---
+++ Esame 27/Summa DList/Summa DList/sum_dlist.h
@@ -0,0 +1,17 @@
+//
+//  sum_dlist.h
+//  Summa DList
+//
+//  Created by Roberto Reggiani on 08/01/24.
+//
+
+#ifndef sum_dlist_h
+#define sum_dlist_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "doublelist.h"
+
+extern Item* DListSum(const Item *i1, const Item *i2);
+
+#endif /* sum_dlist_h */
